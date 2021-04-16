d87d7078574709cebce08651fef051c82bc4695a
yueqiaocertik <yue.qiao@certik.org>
2021-04-16 12:59:35 -0700

    add comments to toSymbols function to make it clear

--- sdk/lib/address.js
+++ sdk/lib/address.js
@@ -2,7 +2,9 @@ const { multiCall } = require("./web3");
 const { decimalify } = require("../util");
 
 async function toSymbols(addresses) {
+  // remove zero address which serves as a address for ETH/BNB
   const queryAddresses = Object.keys(addresses).filter((t) => t !== "0x0000000000000000000000000000000000000000");
+  // use multicall to request symbols of the tokens
   const symbols = (
     await multiCall({
       abi: "bep20:symbol",
@@ -15,10 +17,9 @@ async function toSymbols(addresses) {
       m[t.input.target] = t.output;
       return m;
     },
-    {
-      "0x0000000000000000000000000000000000000000": "BNB",
-    }
+    { "0x0000000000000000000000000000000000000000": "BNB" }
   );
+  // use multicall to request decimals of the tokens
   const decimals = (
     await multiCall(
       "bep20:decimals",
@@ -33,7 +34,7 @@ async function toSymbols(addresses) {
     },
     { "0x0000000000000000000000000000000000000000": 18 }
   );
-
+  // aggregate the symbols and decimals
   const result = Object.keys(addresses).reduce((m, t) => {
     m[symbols[t]] = decimalify(addresses[t], decimals[t]);
     return m;
